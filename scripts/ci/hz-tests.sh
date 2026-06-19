3bcbf60fbd3339703b77be8ebc3a36166a38bc45
yuanyuyuan <az6980522@gmail.com>
2026-06-19 10:56:37 +0000

    test(hiroz-tests): run cyclone-hz-check as part of hz-tests CI

--- scripts/ci/hz-tests.sh
+++ scripts/ci/hz-tests.sh
@@ -2,3 +2,4 @@
 set -e
 cargo build -p hiroz-union -p hiroz-meter --release -j4
 cargo test -p hiroz-tests --test hz_accuracy --features hz-comparison-tests,jazzy --release -- --nocapture
+bash scripts/ci/cyclone-hz-check.sh
